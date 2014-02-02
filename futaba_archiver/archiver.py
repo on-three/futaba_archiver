6bbdb792a1be989ccc8066d970f4ec5fe058ed60
on-three <on_three@outlook.com>
2014-02-01 16:23:22 -0800

    Moved scheduler to simpler single thread model.

--- futaba_archiver/archiver.py
+++ futaba_archiver/archiver.py
@@ -25,30 +25,43 @@ from apscheduler.scheduler import Scheduler
 from futaba_scrape import get_threads
 from futaba_scrape import Post
 
+try:
+  #we try to access django settings for the web interface to access
+  #the samed databas (via django settings file)
+  
+  os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webserver.settings")
+  from futaba_archive import models as bdmodels                  
+except ImportError:
+  print 'Could not specify DJANGO_SETTINGS_MODULE via django settings module. Failing'
+  sys.exit(-1)
+
 #TODO: Store boards to archive in db
-boards = [ 
+urls = [ 
   'http://dat.2chan.net/img2/futaba.htm', #nijigen gazou keijiban ('2D' Image Board)
+  'http://dec.2chan.net/b/futaba.htm', #nijigen ura '/b/' board ('Alt 2D')
   ]
 
-def update_archive():
-  print '------------------------------------------------'
-  threads = get_threads(boards[0])
+def update_archive(url):
+  print '----{url}----'.format(url=url)
+  threads = get_threads(url)
   for thread in threads:
     print unicode(thread)
+  print '----{url}----'.format(url=url)
 
 def main():
   parser = argparse.ArgumentParser(description='Crawl futaba board, updating archive from boards every X minutes')
   #parser.add_argument('-u', '--url',  help='url to fetch for scraping.', type=str, default='http://dat.2chan.net/img2/futaba.htm')
   args = parser.parse_args()
 
-  scheduler = Scheduler()
+  #starting scheduler in standalone mode. All jobs run in this thread.
+  #Allows us to sequentially check N boards in sequence.
+  scheduler = Scheduler(standalone=True)
+
+  for url in urls:
+    scheduler.add_interval_job(update_archive, minutes=3, kwargs={'url': url})
 
   scheduler.start()
 
-  scheduler.add_interval_job(update_archive, minutes=3)
-
-  while True:
-    pass
 
 if __name__ == "__main__":
   main()
